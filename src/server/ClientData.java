7f17cbfca4a8c7076bc97d517f4ffaff0e811a32
Bruno Boaventura de Oliveira Lacerda <brunoboaventura@gmail.com>
2017-07-29 02:05:51 -0300

    Mais estruturação do código

--- src/server/ClientData.java
+++ src/server/ClientData.java
@@ -1,18 +1,30 @@
 package server;
 
 import java.io.Serializable;
-import java.net.InetAddress;
 import java.net.Socket;
 
 public class ClientData implements Serializable {
 
-    private String nickname;
-    private Socket socketTCP;
-    private String ip;
+    private final String nickname;
+    private final Socket socketTCP;
+    private final String ip;
    
     public ClientData (String n, Socket s, String ip) {
         this.nickname = n;
         this.socketTCP = s;
         this.ip = ip;
     }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public Socket getSocketTCP() {
+        return socketTCP;
+    }
+
+    public String getIp() {
+        return ip;
+    }    
+    
 }
