328ff62c4c77146ae2d9cd07fd8a67b6e9108a0f
Bruno Boaventura de Oliveira Lacerda <brunoboaventura@gmail.com>
2017-08-01 02:40:42 -0300

    só falta a transferência do arquivo.

--- src/client/Chat.java
+++ src/client/Chat.java
@@ -1,8 +1,10 @@
 package client;
 
+import java.io.IOException;
+
 public class Chat {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
